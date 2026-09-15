30fbd41a2eadfa09156168b70a98e42caf43670b
Murilo von Baranow <mvonbaranow@gmail.com>
2026-09-15 08:51:01 -0300

    Cors implementado

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./database');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
